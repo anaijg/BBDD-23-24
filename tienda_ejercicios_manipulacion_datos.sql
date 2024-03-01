bd4e8af437daf77a85fcb758c7d5dec10d3b02bc
anaijg <ana.jimenezgomez@educa.madrid.org>
2024-03-01 10:42:15 +0100

    Enunciados ejercicios tienda manipulación de datos

---
+++ tienda_ejercicios_manipulacion_datos.sql
@@ -0,0 +1,31 @@
+# https://josejuansanchez.org/bd/unidad-11-teoria/index.html#ejercicios-pr%C3%A1cticos
+
+# CADA VEZ QUE VAYAS A REALIZAR ESTOS EJERCICIOS ACUÉRDATE DE RESTAURAR LA BASE DE DATOS tienda A SU CONFIGURACIÓN ORIGINAL CON https://github.com/anaijg/BBDD-23-24/blob/main/tienda_esquema_y_datos.sql
+
+# 7.1 Tienda de informática
+# Realice las siguientes operaciones sobre la base de datos tienda.
+
+# 01.- Inserta un nuevo fabricante indicando su código y su nombre.
+
+# 02.- Inserta un nuevo fabricante indicando solamente su nombre.
+
+# 03.- Inserta un nuevo producto asociado a uno de los nuevos fabricantes. La sentencia de inserción debe incluir: código, nombre, precio y código_fabricante.
+
+# 04.- Inserta un nuevo producto asociado a uno de los nuevos fabricantes. La sentencia de inserción debe incluir: nombre, precio y código_fabricante.
+
+# 05.- Crea una nueva tabla con el nombre fabricante_productos que tenga las siguientes columnas: nombre_fabricante, nombre_producto y precio. Una vez creada la tabla inserta todos los registros de la base de datos tienda en esta tabla haciendo uso de única operación de inserción.
+
+# 6.- Crea una vista con el nombre vista_fabricante_productosque tenga las siguientes columnas: nombre_fabricante, nombre_producto y precio.
+
+# 07.- Elimina el fabricante Asus. ¿Es posible eliminarlo? Si no fuese posible, ¿qué cambios debería realizar para que fuese posible borrarlo?
+
+# 08.- Elimina el fabricante Xiaomi. ¿Es posible eliminarlo? Si no fuese posible, ¿qué cambios debería realizar para que fuese posible borrarlo?
+
+# 09.- Actualiza el código del fabricante Lenovo y asígnale el valor 20. ¿Es posible actualizarlo? Si no fuese posible, ¿qué cambios debería realizar para que fuese actualizarlo?
+
+# 10.- Actualiza el código del fabricante Huawei y asígnale el valor 30. ¿Es posible actualizarlo? Si no fuese posible, ¿qué cambios debería realizar para que fuese actualizarlo?
+
+# 11.- Actualiza el precio de todos los productos sumándole 5 € al precio actual.
+
+# 12.- Elimina todas las impresoras que tienen un precio menor de 200 €.
+
